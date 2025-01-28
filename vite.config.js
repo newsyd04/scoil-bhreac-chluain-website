a432bdbf90552afaeae59fd31e911b8b7382c8ea
newsyd04 <newsyd04@gmail.com>
2025-01-28 11:28:56 +0000

    fix github pages integration

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   css: {
     postcss: "./postcss.config.js", // Ensure this points to your PostCSS config
   },
-  base: "/scoil-bhreac-chluain-website/", 
+  base: "https://newsyd04.github.io/scoil-bhreac-chluain-website/", 
 });
