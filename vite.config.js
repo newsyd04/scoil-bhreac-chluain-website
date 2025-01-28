7d0b5be2ff5b238c5bef3656b5806c19a523a4f8
newsyd04 <newsyd04@gmail.com>
2025-01-28 12:21:34 +0000

    fix github pages integration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{ src: "dist/index.html", dest: "404.html" }], // Redirect 404s to index.html for GitHub Pages
-    }),
-  ],
+  plugins: [react()],
   css: {
     postcss: "./postcss.config.js",
   },
