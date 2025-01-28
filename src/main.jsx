f6c83217bbea9c80d8516a0f396107089c57e546
newsyd04 <newsyd04@gmail.com>
2025-01-28 11:47:45 +0000

    fix github pages integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? "/scoil-bhreac-chluain-website" : "/";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename={basename}>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
+
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: "./postcss.config.js", // Ensure this points to your PostCSS config
+    postcss: "./postcss.config.js",
   },
-  base: "/scoil-bhreac-chluain-website/", 
+  base: isProduction ? "/scoil-bhreac-chluain-website/" : "/", // Use "/" for development
 });
